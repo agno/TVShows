1017e8aadf6d99609fa86e7a0c1bebfd91091c36
mattprice <mattprice91@gmail.com>
2009-09-16 17:49:12 -0400

    Upgraded to GNU GPL v3

--- Controller.m
+++ Controller.m
@@ -1,7 +1,11 @@
 /*
  This file is part of the TVShows source code.
- http://tvshows.sourceforge.net
- It may be used under the terms of the GNU General Public License.
+ http://github.com/mattprice/TVShows
+
+ TVShows is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 */
 
 #import "Controller.h"
@@ -49,7 +53,7 @@
 #define RubyGetShowDetails			@"GetShowDetails"
 
 // Misc
-#define TVShowsURL					@"http://tvshows.sourceforge.net"
+#define TVShowsURL					@"http://www.deathtobunnies.com/tvshows/"
 #define TVShowsFeedbackURL			@"http://sourceforge.net/tracker/?group_id=190705"
 #define TransmissionURL				@"http://transmission.m0k.org"
 
--- DownloadShowList.rb
+++ DownloadShowList.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 
 # This file is part of the TVShows source code.
-# http://tvshows.sourceforge.net
-# It may be used under the terms of the GNU General Public License.
+# http://github.com/mattprice/TVShows
+
+# TVShows is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
 requires = [
 	'open-uri',
