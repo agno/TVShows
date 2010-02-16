989c5491ca6745754be4f764c6c16168174710d6
mattprice <mattprice91@gmail.com>
2010-02-16 09:43:42 -0500

    Blocked misnumbered episode: `Family Guy 8x20`

--- TVShowsScript/TVShows.rb
+++ TVShowsScript/TVShows.rb
@@ -56,7 +56,7 @@ class Episode
 	end
 	
 	def download
-	  blockedTorrents = ["http://torrent.zoink.it/CSI.New.York.S07E03.HDTV.XviD-LOL.%5Beztv%5D.torrent", "http://www.bt-chat.com/download1.php?info_hash=6f22dae012c3563508141cbc6fec3fcffd9e8cd3", "http://torrent.zoink.it/Doctor.Who.2005.The.Waters.Of.Mars.2009.Special.HDTV.XviD-FoV.%5Beztv%5D.torrent", "http://torrent.zoink.it/Doctor.Who.2005.The.Waters.Of.Mars.2009.Special.720p.HDTV.x264-FoV.%5Beztv%5D.torrent", "http://torrent.zoink.it/Doctor.Who.2005.S04.Special.The.End.Of.Time.Part1.720p.HDTV.x264-BiA.%5Beztv%5D.torrent","http://torrent.zoink.it/Doctor.Who.2005.S04.Special.The.End.Of.Time.Part2.720p.HDTV.x264-BiA.%5Beztv%5D.torrent"]
+	  blockedTorrents = ["http://torrent.zoink.it/CSI.New.York.S07E03.HDTV.XviD-LOL.%5Beztv%5D.torrent", "http://www.bt-chat.com/download1.php?info_hash=6f22dae012c3563508141cbc6fec3fcffd9e8cd3", "http://torrent.zoink.it/Doctor.Who.2005.The.Waters.Of.Mars.2009.Special.HDTV.XviD-FoV.%5Beztv%5D.torrent", "http://torrent.zoink.it/Doctor.Who.2005.The.Waters.Of.Mars.2009.Special.720p.HDTV.x264-FoV.%5Beztv%5D.torrent", "http://torrent.zoink.it/Doctor.Who.2005.S04.Special.The.End.Of.Time.Part1.720p.HDTV.x264-BiA.%5Beztv%5D.torrent","http://torrent.zoink.it/Doctor.Who.2005.S04.Special.The.End.Of.Time.Part2.720p.HDTV.x264-BiA.%5Beztv%5D.torrent", "http://torrent.zoink.it/Family.Guy.S08E20.DVDRip.XviD-REWARD.%5Beztv%5D.torrent"]
 
 	  if ( blockedTorrents.include?(@torrentURL) )
 			printError("The torrent #{@torrentURL} was blocked.")
